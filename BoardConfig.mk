0972989ce0549b4020f73176e1dd09536accdaac
lamjiidii1 <lamjiidii1@gmail.com>
2021-04-20 20:13:05 +0000

    cepheus: remove FM from Board

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,9 +83,6 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
-# FM
-BOARD_HAVE_QCOM_FM := true
-
 # FOD
 TARGET_SURFACEFLINGER_FOD_LIB := \
     //$(DEVICE_PATH):libfod_extension.cepheus
