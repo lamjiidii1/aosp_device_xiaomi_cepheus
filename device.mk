12f247cbf77a23c954abc5d82daf6b507b4e7baf
lamjiidii1 <lamjiidii1@gmail.com>
2021-04-25 23:24:21 +0000

    cepheus: Add ArrowOS specific overlay stuff

--- device.mk
+++ device.mk
@@ -38,7 +38,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-arrow
 
 # AID/fs configs
 PRODUCT_PACKAGES += \
