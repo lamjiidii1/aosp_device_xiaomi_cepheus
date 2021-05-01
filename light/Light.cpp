4c50da5a8aec35e9476e55e1517d7c2a8a9ddf4e
lamjiidii1 <lamjiidii1@gmail.com>
2021-05-01 16:22:36 +0000

    cepheus: light: page404

--- light/Light.cpp
+++ light/Light.cpp
@@ -22,9 +22,15 @@
 
 #include <fstream>
 
+<<<<<<< HEAD
 #define LCD_LED "/sys/class/backlight/panel0-backlight/"
 //#define BLUE_LED        "/sys/class/leds/blue/"  //Right LED
 #define GREEN_LED "/sys/class/leds/green/"  // Left LED
+=======
+#define LCD_LED         "/sys/class/backlight/panel0-backlight/"
+#define BLUE_LED        "/sys/class/leds/blue/"
+#define GREEN_LED       "/sys/class/leds/green/"
+>>>>>>> parent of 8e57040... raphael: lights: Change LED behavior
 
 #define BREATH "breath"
 #define BRIGHTNESS "brightness"
@@ -92,9 +98,13 @@ static void handleBacklight(const LightState& state) {
 }
 
 static void handleNotification(const LightState& state) {
+    uint32_t blueBrightness = getScaledBrightness(state, MAX_LED_BRIGHTNESS);
     uint32_t greenBrightness = getScaledBrightness(state, MAX_LED_BRIGHTNESS);
 
     /* Disable breathing or blinking */
+    set(BLUE_LED BREATH, 0);
+    set(BLUE_LED DELAY_OFF, 0);
+    set(BLUE_LED DELAY_ON, 0);
 
     set(GREEN_LED BREATH, 0);
     set(GREEN_LED DELAY_OFF, 0);
@@ -102,16 +112,24 @@ static void handleNotification(const LightState& state) {
 
     switch (state.flashMode) {
         case Flash::HARDWARE:
+<<<<<<< HEAD
             /* Breathing */
+=======
+            /* Breathing */  
+            set(BLUE_LED BREATH, 1);
+>>>>>>> parent of 8e57040... raphael: lights: Change LED behavior
             set(GREEN_LED BREATH, 1);
             break;
         case Flash::TIMED:
             /* Blinking */
+            set(BLUE_LED DELAY_OFF, state.flashOnMs);
+            set(BLUE_LED DELAY_ON, state.flashOffMs);
             set(GREEN_LED DELAY_OFF, state.flashOnMs);
             set(GREEN_LED DELAY_ON, state.flashOffMs);
             break;
         case Flash::NONE:
         default:
+            set(BLUE_LED BRIGHTNESS, blueBrightness);
             set(GREEN_LED BRIGHTNESS, greenBrightness);
     }
 }
