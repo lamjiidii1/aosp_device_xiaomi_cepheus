0084672667bb1dbb841cf4b500dfb101a73c1551
lamjiidii1 <lamjiidii1@gmail.com>
2021-05-01 17:09:22 +0000

    cepheus: sepolicy: rename to cepheus

--- device.mk
+++ device.mk
@@ -274,7 +274,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.raphael
+    android.hardware.light@2.0-service.xiaomi_msmnile
 
 # Media
 PRODUCT_PACKAGES += \
